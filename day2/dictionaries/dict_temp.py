f891572f6bb3ee029ea6811d1ce1672f1caea92b
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2021-08-26 01:39:18 -0600

    Add files via upload

---
+++ day2/dictionaries/dict_temp.py
@@ -0,0 +1,7 @@
+avengers = {'ironman':'suit', 'captain':'shield', 'thor':'hammer'}
+
+print(list(avengers.keys()))
+print("----")
+print(list(avengers.values()))
+print("----")
+print(list(avengers.items()))
